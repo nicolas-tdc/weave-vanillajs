9175a001b3b3d336136fb35c1dad8a72947963b8
Nicolas Tachet <nicolas.tachetdescombes@gmail.com>
2025-03-01 15:57:02 +0100

    Initial commit

---
+++ build/app.js
@@ -0,0 +1,8 @@
+function fetchData() {
+    document.getElementById("output").textContent = "toto";
+    // fetch("/api/data")
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         document.getElementById("output").textContent = data.message;
+    //     });
+}
