e4400b45be2bcc7d679f0813eff6ac90cda2b619
Nicolas Tachet <nicolas.tachetdescombes@gmail.com>
2025-03-02 02:00:14 +0100

    Fix run and move docker install to deploy-sk

--- scripts/init.sh
+++ scripts/init.sh
@@ -4,9 +4,3 @@
 set -e
 
 echo -e "\e[33mInitializing vanillajs-sk...\e[0m"
-
-# Install required packages
-if [ -f ".scripts/helpers/installed-required.sh" ]; then
-    echo -e "\e[33mInstalling required packages...\e[0m"
-    .scripts/helpers/install-required.sh
-fi
--- scripts/run.sh
+++ scripts/run.sh
@@ -5,12 +5,6 @@ set -e
 
 echo -e "\e[33mRunning vanillajs-sk...\e[0m"
 
-# Install required packages
-if [ -f "./scripts/helpers/installed-required.sh" ]; then
-    echo -e "\e[33mInstalling required packages...\e[0m"
-    ./scripts/helpers/install-required.sh
-fi
-
 if [ $APP_ENV == "dev" ] && [ -f ".env.dev" ]; then
     cp .env.dev .env
 fi
@@ -19,8 +13,6 @@ if [ $APP_ENV == "prod" ] && [ -f ".env.prod" ]; then
     cp .env.prod .env
 fi
 
-# run.sh
-
 # Stopping existing containers
 echo -e "\e[33mStopping existing containers...\e[0m"
 docker-compose down
@@ -28,7 +20,6 @@ docker-compose down
 # Building and starting containers
 echo -e "\e[33mBuilding and starting container...\e[0m"
 docker-compose up --build -d
-
 
 # Cleaning up unused Docker images
 echo -e "\e[33mCleaning up unused Docker images...\e[0m"
